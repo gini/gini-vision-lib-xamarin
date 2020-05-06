7f7a0f0c79ed7ea961f4bcc8ebbbba35a2d5d558
maciejtrybilo <maciej@gini.net>
2020-05-06 17:09:10 +0200

    Automated iOS dll creation.

---
+++ GVLXamarinBindingsiOS/GVLProxy/GVLProxy/GVLProxy.h
@@ -0,0 +1,19 @@
+//
+//  GVLProxy.h
+//  GVLProxy
+//
+//  Created by Maciej Trybilo on 16.04.20.
+//  Copyright © 2020 Gini GmbH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GVLProxy.
+FOUNDATION_EXPORT double GVLProxyVersionNumber;
+
+//! Project version string for GVLProxy.
+FOUNDATION_EXPORT const unsigned char GVLProxyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GVLProxy/PublicHeader.h>
+
+
